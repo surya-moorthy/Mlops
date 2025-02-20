b59785c243ded25284a1b66cac7ae9f234830f4f
SATHYAMOORTHY TS <sathyamoortthyts0105@gmail.com>
2025-02-20 12:20:17 +0530

    saved and loaded the model using joblib

---
+++ main.py
@@ -0,0 +1,6 @@
+from model.train import cfl
+
+
+
+
+
--- model/train.py
+++ model/train.py
@@ -1,18 +1,20 @@
+from sklearn.model_selection import train_test_split , GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+import pandas as pd
 
+data = pd.read_csv("house.csv")
+data.dropna(inplace=True)
 
-# model = RandomForestRegressor(n_estimators=100,random_state=42)
+x , y = data.drop(columns=["MEDV"]) , data["MEDV"]
 
-# model.fit(x_train,y_train)
+x_train , x_test , y_train , y_test =  train_test_split(x,y,test_size=0.2,random_state=42)
 
-# y_pred = model.predict(x_test)
+forest_params = [{'max_depth': list(range(10, 15)), 'max_features': list(range(1,10))}]
 
-# mse = mean_squared_error(y_test,y_pred)
+clf = GridSearchCV(RandomForestRegressor(), forest_params, cv = 5, scoring='neg_mean_squared_error')
 
+clf.fit(x_train, y_train)
 
-
-
-
-
-
-
-
+print(clf.best_params_)
+print(clf.best_score_)
