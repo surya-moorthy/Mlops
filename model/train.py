b78f650fbcc25be1fa1b694c887380f00041d045
SATHYAMOORTHY TS <sathyamoortthyts0105@gmail.com>
2025-02-20 10:25:16 +0530

    Trained a model with housing dataset

---
+++ model/train.py
@@ -0,0 +1,18 @@
+
+
+# model = RandomForestRegressor(n_estimators=100,random_state=42)
+
+# model.fit(x_train,y_train)
+
+# y_pred = model.predict(x_test)
+
+# mse = mean_squared_error(y_test,y_pred)
+
+
+
+
+
+
+
+
+
